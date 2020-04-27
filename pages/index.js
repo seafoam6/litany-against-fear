a0c9895397102361b686ca57787bcc2058c6f780
Steve Barman <steve@stevebarman.com>
2020-04-27 11:56:10 -0400

    adds jest, adds typescript

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import litany from '../data/litany'
 
 export default function Home() {
   return (
---
+++ phases/components/__tests__/CharCube.test.ts
@@ -0,0 +1,6 @@
+describe('something', () => {
+  it('thing', () => {
+
+    expect(0).toEqual(0)
+  })
+})
